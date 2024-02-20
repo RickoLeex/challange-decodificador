28424760e0b690d3ab5ca4a550797db477c7692b
Wérick Santos <wrick.goncalves@aluno.enova.educacao.ba.gov.br>
2024-02-20 20:10:17 -0300

    Atualização do back-end das funções de criptografia.

---
+++ app.js
@@ -0,0 +1,63 @@
+function Criptografar() {
+    let texto = document.getElementById("texto").value;
+
+    //separando letras
+    let letras = texto.split('');
+    
+    //verifica e substitui letras
+    letras.forEach(letra => {
+        if(letra == "e"){
+            let posicao = letras.indexOf(letra);
+            letras.fill("enter", posicao, posicao + 1);
+        }
+        if(letra == "i"){
+            let posicao = letras.indexOf(letra);
+            letras.fill("imes", posicao, posicao + 1);
+        }
+        if(letra == "a"){
+            let posicao = letras.indexOf(letra);
+            letras.fill("ai", posicao, posicao + 1);
+        }
+        if(letra == "o"){
+            let posicao = letras.indexOf(letra);
+            letras.fill("ober", posicao, posicao + 1);
+        }
+        if(letra == "u"){
+            let posicao = letras.indexOf(letra);
+            letras.fill("ufat", posicao, posicao + 1);
+        }
+    });
+
+    //Transforma a array em string para poder retornar.
+    texto = letras.toString();
+    texto = texto.replace(/,/g, '')
+    document.getElementById("texto").value = texto;
+}
+
+function Descriptografar() {
+    let texto = document.getElementById("texto").value;
+    console.log(texto);
+    let letras = texto.split('');
+    let index = 0;
+
+    while (index < letras.length) {
+        let letra = letras[index];
+
+        if (letra == "e" && letras.slice(index, index + 5).join('') == "enter") {
+            letras.splice(index + 1, 4);
+        } else if (letra == "i" && letras.slice(index, index + 4).join('') == "imes") {
+            letras.splice(index + 1, 3);
+        } else if (letra == "a" && letras.slice(index, index + 2).join('') == "ai") {
+            letras.splice(index + 1, 1);
+        } else if (letra == "o" && letras.slice(index, index + 4).join('') == "ober") {
+            letras.splice(index + 1, 3);
+        } else if (letra == "u" && letras.slice(index, index + 4).join('') == "ufat") {
+            letras.splice(index + 1, 3);
+        } else {
+            index++;
+        }
+    }
+    texto = letras.toString();
+    texto = texto.replace(/,/g, '')
+    document.getElementById("texto").value = texto;
+}
